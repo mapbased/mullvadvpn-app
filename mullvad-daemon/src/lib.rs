9fb62044b2f4607b21a521ad9df78ee398bc3b9d
Emīls <emils@mullvad.net>
2021-09-06 12:09:47 +0100

    Add ObfuscatorConfig and Type

--- mullvad-daemon/src/lib.rs
+++ mullvad-daemon/src/lib.rs
@@ -1147,6 +1147,7 @@ where
                     },
                     options: tunnel_options.wireguard.options,
                     generic_options: tunnel_options.generic,
+                    obfuscation: None,
                 }
                 .into())
             }
